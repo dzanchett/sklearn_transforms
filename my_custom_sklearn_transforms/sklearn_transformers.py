a0d34a11054d30923de52a78e788774ae797bfe5
dzanchett <50345806+dzanchett@users.noreply.github.com>
2020-08-26 00:19:22 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,10 +1,10 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-import pandas as pd
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
+        import pandas as pd
         self.columns = columns
 
     def fit(self, X, y=None):
